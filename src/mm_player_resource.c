008641bd919f4a4e26269e8412fbe7339643b67d
Sangchul Lee <sc11.lee@samsung.com>
2016-07-07 10:17:23 +0900

    Disable waiting logic for finishing resource acquisition
    Change-Id: Ia36a07c93b1a329a1fb2c7633152e3c4854d6133

--- src/mm_player_resource.c
+++ src/mm_player_resource.c
@@ -413,6 +413,7 @@ int _mmplayer_resource_manager_acquire(MMPlayerResourceManager *resource_manager
 				LOGE("- could not acquire resource, ret(%d)\n", ret);
 				ret = MM_ERROR_RESOURCE_INTERNAL;
 			}
+#if 0 /* Fix me: Disable below logic temporarily */
 			else
 			{
 				gint64 end_time = g_get_monotonic_time() + MMPLAYER_RESOURCE_ACQUIRE_TIMEOUT*G_TIME_SPAN_SECOND;
@@ -426,6 +427,7 @@ int _mmplayer_resource_manager_acquire(MMPlayerResourceManager *resource_manager
 					LOGD("- resources are acquired\n");
 				}
 			}
+#endif
 
 			MMPLAYER_RESOURCE_UNLOCK(resource_manager);
 		}
